69629d7ecc2ee6886d83afe1323a3b926841c986
angelo <angelo@PCAPERNIA.CJPB>
2016-11-21 14:24:10 -0400

    Cambios en Barra de Navegacion

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -189,7 +189,7 @@
     <div class="body">
 	
 	<div class="site-header-wrapper">
-        <header class="site-header" style="background-color: #2F2E2E;">
+        <header class="site-header header-navbar" style="background-color: #2F2E2E;">
             <div class="container sp-cont" >
                 <div class="site-logo">
                     <h1><a href="{{ route ('home') }}"><img src="{{ asset('autostars/images/trovami-logo-beta.png') }}" alt="Logo"></a></h1>
@@ -406,11 +406,8 @@
 
                     <div class="topnav dd-menu">
                         <ul class="top-navigation sf-menu">
-                            <li class="button-register-original">
+                            <li>
                                 <a href="{{route('register')}}" style="color:#FFF;">Registrate</a>
-                            </li>
-                            <li class="button-register-responsive">
-                            	<a class="user-login-btn" href="{{route('register')}}" style="color:#FFF;"><i class="fa fa-user-plus"></i></a>
                             </li>
                         </ul>
 
@@ -436,6 +433,70 @@
                 </div>         
             @endif
 
+            <div class="header-right">
+
+                @if (Auth::guest())
+
+                    <div class="user-login-panel user-button" style="color:#FFF;">
+                    
+                        <a href="#" class="user-login-btn" data-toggle="modal" data-target="#loginModal">
+                            <i class="fa fa-user" style="color:#FFF;"></i>
+                        </a>
+
+                    </div>
+
+                @endif
+
+                @if (Auth::user())
+                    <div class="user-login-panel logged-in-user">
+                        <a href="#" class="user-login-btn" id="userdropdown" data-toggle="dropdown">
+
+                            @if (Auth::user()->blb_img != "")
+                            	<img src="data:image/jpeg;base64,{{ Auth::user()->blb_img }}" alt="">
+							@else
+						
+								  @if (Auth::user()->lng_idgenero == 1)
+								  	<img src="{{ asset('autostars/images/user_masculino.png') }}" alt="">								  	
+								  @elseif (Auth::user()->lng_idgenero == 2)
+								  	<img src="{{ asset('autostars/images/usuario_femenino.png') }}" alt="">
+								  @endif								
+
+                       		@endif
+
+                            <span class="user-informa">
+                                <span class="meta-data" style="color:#FFF;">Bienvenido</span>
+                                <span class="user-name">{{ Auth::user()->name }}</span>
+                            </span>
+                            <span class="user-dd-dropper"><i class="fa fa-angle-down"></i></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu" aria-labelledby="userdropdown">
+                            <li><a href="{{route('cuenta')}}"> <i class="fa fa-cog"></i> Mi Cuenta</a></li>
+                            <li><a href="{{route('publicar')}}"><i class="fa fa-newspaper-o" aria-hidden="true"></i> Publicar</a></li>
+                            <!-- <li><a href="#"><i class="fa fa-lightbulb-o" aria-hidden="true"></i> Sugerencias</a></li>-->                            
+                            <li><a href="{{route('logout')}}"><i class="fa fa-sign-out"></i> Salir</a></li>
+                        </ul>
+                    </div>
+                @endif
+
+				@if (Auth::guest())
+
+                    <div class="topnav dd-menu">
+                        <ul class="top-navigation sf-menu">
+                            <li class="button-register-original">
+                                <a href="{{route('register')}}" style="color:#FFF;">Registrate</a>
+                            </li>
+                            <li class="button-register-responsive">
+                            	<a class="user-login-btn" href="{{route('register')}}" style="color:#FFF;"><i class="fa fa-user-plus"></i></a>
+                            </li>
+                        </ul>
+
+                    </div>
+                    
+ 				@endif
+                    
+                    
+                </div>
                 <a href="#" class="visible-sm visible-xs" id="menu-toggle"><i class="fa fa-bars"></i></a>
                 <!-- Main Navigation -->
                 <nav class="main-navigation dd-menu toggle-menu" role="navigation">
@@ -443,108 +504,16 @@
                         <li>
                         	<a href="{{ route ('home') }}"><i class="fa fa-home" aria-hidden="true"></i> Inicio</a>                        
                         </li>
-
-
-<!-- 
-                        <li class="megamenu"><a href="#"><i class="fa fa-coffee" aria-hidden="true"></i> Servicios</a>
-                            <ul class="dropdown">
-                                <li>
-                                    <div class="megamenu-container container">
-                                        <div class="row">
-                                            <div class="mm-col col-md-2">
-                                                <ul class="sub-menu">
-                                                    <li><a href="{{ route('accesorios') }}">Accesorios</a></li>
-                                                    <li><a href="{{ route('repuestos') }}">Repuestos</a></li>
-                                                   
-                                                    <li><a href="{{ route('talleres') }}">Talleres</a></li>
-                                                    <li><a href="{{ route('concesionarios') }}">Concesionarios</a></li>
-                                                    <li><a href="#">Seguros</a></li>
-                                                    <li><a href="#">Eventos</a></li>
-                                                </ul>
-                                            </div>
-                                            <div class="mm-col col-md-5">
-                                                <span class="megamenu-sub-title">Espacio Publicitario</span>
-													<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-													  <!-- Indicators --
-													  <ol class="carousel-indicators">
-													    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-													    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-													    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-													  </ol>
-													
-													  <!-- Wrapper for slides --
-													  <div class="carousel-inner" role="listbox">
-													    <div class="item active">
-													      <img src="" alt="...">
-													      <!-- 
-													      <div class="carousel-caption">
-													        ...
-													      </div>
-													       --
-													    </div>
-													    <div class="item">
-													      <img src="" alt="...">
-													      <!-- 
-													      <div class="carousel-caption">
-													        ...
-													      </div>
-													       --
-													    </div>
-													    <div class="item">
-													      <img src="" alt="...">
-													      
-													      <!-- 
-													      <div class="carousel-caption">
-													        ...
-													      </div>
-													       --
-													      
-													    </div>    
-													    <!-- ...  --
-													  </div>
-													
-													  <!-- Controls --
-													  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-													    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-													    <span class="sr-only">Previous</span>
-													  </a>
-													  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-													    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-													    <span class="sr-only">Next</span>
-													  </a>
-													</div>
-                                                
-                                            </div>
-                                            <div class="mm-col col-md-5">
-                                                <span class="megamenu-sub-title">Los más solicitados</span>
-                                                <ul class="make-widget">
-                                                    <li class="item"> <a href="#"><img src="" alt=""></a></li>
-                                    				<li class="item"> <a href="#"><img src="" alt=""></a></li>
-                                    				<li class="item"> <a href="#"><img src="" alt=""></a></li>
-                                                    <li class="item"> <a href="#"><img src="" alt=""></a></li>
-                                                    <li class="item"> <a href="#"><img src="" alt=""></a></li>
-                                                    <li class="item"> <a href="#"><img src="" alt=""></a></li>
-                                                    <li class="item"> <a href="#"><img src="" alt=""></a></li>
-                                    				<li class="item"> <a href="#"><img src="" alt=""></a></li>
-                                                </ul>
-                                                <a href="#" class="basic-link">ver todos</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </li>
-
-			-->
-
-
                         <li><a href="{{ route ('quienes_somos') }}"><i class="fa fa-universal-access" aria-hidden="true"></i> Quiénes Somos</a></li>
                         <li><a href="{{ route ('soporte') }}"><i class="fa fa-life-ring"></i> Soporte</a></li>
                         <li><a href="{{ route ('contacto') }}"><i class="fa fa-envelope"></i> Contacto</a></li>
-                        
-                        
                     </ul>
-                </nav> 
+                </nav>
+                <div class="site-logo">
+                    <h1 class="logo-original"><a href="{{ route ('home') }}"><img src="{{ asset('autostars/images/trovami-logo-beta.png') }}" alt="Logo"></a></h1>
+                    <h1 class="isotipo-original"><a href="{{ route ('home') }}"><img src="{{ asset('autostars/images/troovami-vehicles.png') }}" alt="Logo"></a></h1>
+                   <!-- <span class="site-tagline">Comprar o vender<br>ahora es más fácil!</span>-->
+                </div>
 
                  <div class="container">
                     @if (Session::has('errors'))
